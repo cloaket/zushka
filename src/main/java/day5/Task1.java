f5b9bb3f4ff3cf27dd7b0f9edab7bb6140e9f70f
zushka <zushk@mail.ru>
2021-04-18 00:00:35 +0300

    added day 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -1,7 +1,49 @@
+// Создать класс Автомобиль (англ. Car) с полями “Год выпуска”, “Цвет”, “Модель”.
+// Создать get и set методы для каждого поля. Создать экземпляр класса Автомобиль.
+// Задать значение для каждого поля, используя set методы.
+// Вывести в консоль значение каждого  из полей, используя get методы.
+// Созданный вами класс должен отвечать принципам инкапсуляции.
+
 package day5;
 
 public class Task1 {
     public static void main(String[] args) {
-
+        Car car = new Car();
+        car.setColor("red");
+        car.setModel("Porshe 911");
+        car.setYear(1994);
+        System.out.println("Цвет : " + car.getColor());
+        System.out.println("Модель : " + car.getModel());
+        System.out.println("Год выпуска : " + car.getYear());
     }
+}
+
+class Car {
+
+    private String color;
+    private String model;
+    private int year;
+
+
+    public void setColor(String colorCar) {
+        color = colorCar;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void setModel(String colorModel) {
+        model = colorModel;
+    }
+    public String getModel() {
+        return model;
+    }
+
+    public void setYear(int yearCar) {
+        year = yearCar;
+    }
+    public int getYear() {
+        return year;
+    }
+
 }
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -1,8 +1,45 @@
+// Создать класс Мотоцикл (англ. Motorbike) с полями “Год выпуска”, “Цвет”, “Модель”.
+// Создать экземпляр класса Мотоцикл, используя конструктор (set методы не использовать).
+// Необходимо придерживаться принципов инкапсуляции и использовать ключевое слово this.
+// Вывести в консоль значение каждого из полей, используя get методы.
+
 package day5;
 
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike("Yamaha", "Super Soco TC", 1994);
+        motorbike.getColor();
+        motorbike.getModel();
+        motorbike.getYear();
 
     }
+}
+
+class  Motorbike {
+    private String color;
+    private String model;
+    private int year;
+
+    public Motorbike(String yamaha, String super_soco_tc, int i) {
+        System.out.println("Конструктор");
+        this.color = "black";
+        this.model = "Super Soco TC";
+        this.year = 1994;
+
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+
 }
