42e8ffeb2ecab39443055c7ecfb7e0c75c1222d7
zushka <zushk@mail.ru>
2021-04-23 00:22:07 +0300

    added task 1 / day 7

--- src/main/java/day7/Task1.java
+++ src/main/java/day7/Task1.java
@@ -1,7 +1,16 @@
 package day7;
 
+// 1. Для этого задания скопируйте класс Самолет из предыдущего дня в пакет текущего дня.
+// В классе Самолет реализовать статический метод compareAirplanes, который в качестве аргументов
+// принимает два объекта класса Airplane (два самолета) и выводит сообщение в консоль о том,
+// какой из самолетов длиннее.
+
+
+
 public class Task1 {
     public static void main(String[] args) {
-
+        Airplane airplane = new Airplane("Boing",1994, 100, 40);
+        Airplane airplane2 = new Airplane("CY-250",1994, 140, 40);
+        Airplane.compareAirplanes(airplane, airplane2);
     }
 }
