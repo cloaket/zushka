b7a2c655f49c9733b2eb42e159878a4c20e8373f
Igor Ilin <cloaket@gmail.com>
2025-03-11 01:00:41 +0300

    Изменена формула

--- src/main/java/day1/Task1.java
+++ src/main/java/day1/Task1.java
@@ -3,7 +3,7 @@ package day1;
 public class Task1 {
     public static void main(String[] args) {
         int number = 0;
-        while (number <= 9){
+        while (number < 10){
             System.out.print("JAVA ");
             number += 1;
         }
